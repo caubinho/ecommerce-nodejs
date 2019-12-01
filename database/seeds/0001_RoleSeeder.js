7157fc92eec42c94d319846dc5f3645863eb6618
Caubinho Pacheco <caubinho@gmail.com>
2019-12-01 09:27:05 +0100

    Rle and Client seed

---
+++ database/seeds/0001_RoleSeeder.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RoleSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Role = use('Role')
+
+class RoleSeeder {
+  async run () {
+
+    //cria role admin
+    await Role.create({
+      name: 'Admin',
+      slug: 'admin',
+      description: 'Administrador do sistema'
+    })
+
+    //cria role gerente
+    await Role.create({
+      name: 'Manager',
+      slug: 'manager',
+      description: 'Gerente do sistema'
+    })
+
+    //cria cliente
+    await Role.create({
+      name: 'Client',
+      slug: 'client',
+      description: 'Cliente da loja'
+    })
+
+
+
+  }
+}
+
+module.exports = RoleSeeder
