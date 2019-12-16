1c3038e246b413e9fd987c5c99d15cbd823a0ccd
Caubinho <caubinho@gmail.com>
2019-12-17 00:57:42 +0100

    Upload simples

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -34,6 +34,30 @@ const Helpers = use('Helpers')
 
  }
 
+ /**
+  * Move um unico arquivo para um caminho especificado, se nenhum for especificado
+  * @param { FileJar } file o arquivo a ser gerenciado
+  * @param { string } path o caminho para onde arqivo deve ser movido
+  */
+
+  const manage_single_upload = async (file, path = null) => {
+    path = path ? path: Helpers.publicPath('uploads')
+
+    //gera um nome aleatorio
+    const random_name = await str_random(30)
+
+    let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+    //renomeia o arquivo e move ele para o path
+
+    await file.move(path, {
+      name: filename
+    })
+
+    return file
+
+  }
+
  module.exports = {
    str_random
  }
